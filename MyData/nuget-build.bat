3ee24350af4562ecbf4b2fa9c9406a5dfdd9a558
Jan Magne Tjensvold <janmagnet@users.noreply.github.com>
2014-08-29 13:30:42 +0200

    Always build release version with nuget-build.bat

--- MyData/nuget-build.bat
+++ MyData/nuget-build.bat
@@ -3,6 +3,6 @@
 if not exist backup mkdir backup
 if exist *.nupkg move /Y *.nupkg backup
 
-nuget pack -build MyData.csproj
+nuget pack -build MyData.csproj -properties Configuration=Release
 
 pause
